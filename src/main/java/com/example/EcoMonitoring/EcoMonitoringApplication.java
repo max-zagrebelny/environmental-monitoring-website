7b0541efeb615951c1931687ff00a3ee4c533d61
max-zagrebelny <maksimzagrebelnij5@gmail.com>
2022-09-08 17:46:34 +0300

    Project created

---
+++ src/main/java/com/example/EcoMonitoring/EcoMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoMonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMonitoringApplication.class, args);
+	}
+
+}
