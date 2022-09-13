ea0ce727207cf7110ece93a86e4bd9a985541224
max-zagrebelny <maksimzagrebelnij5@gmail.com>
2022-09-13 14:56:10 +0300

    bbb

--- src/main/java/com/example/EcoMonitoring/controller/CityController.java
+++ src/main/java/com/example/EcoMonitoring/controller/CityController.java
@@ -52,6 +52,7 @@ public class CityController {
         elements.add(element2);
         model.addAttribute("elem", elements);
         System.out.println(element2.getNameElement());
+        System.out.println(element2.getNameElement());
         return "home";
 
     }
